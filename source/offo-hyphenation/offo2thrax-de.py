8df7c3f158dab06518048386d1afa5d2ffe561b8
Wincent Balin <wincent.balin@gmail.com>
2019-06-01 13:42:03 +0200

    Second iteration of converting hyphenation patterns
    This iteration fails too, because I used composition of CDRewrites, which led to applying the first rewrite and the proceeding to next characters, whereas the correct approach would be applying all patterns repeatedly on the same string.

--- source/offo-hyphenation/offo2thrax-de.py
+++ source/offo-hyphenation/offo2thrax-de.py
@@ -10,6 +10,7 @@ import zipfile
 import xml
 import xml.sax
 import xml.etree.ElementTree as ET
+import itertools
 
 
 class HyphenMin:
@@ -107,33 +108,36 @@ def convert_to_symbols(s: str) -> str:
 
 
 def thraxified_context(ctx: str) -> str:
-    if len(ctx) >= 5 and ctx.startswith('[BOS]'):
-        bos = '"[BOS]" '
-        ctx = ctx[5:]
-    else:
-        bos = ''
-    if len(ctx) >= 5 and ctx.endswith('[EOS]'):
-        eos = ' "[EOS]"'
-        ctx = ctx[:-5]
-    else:
-        eos = ''
-    syms = []
-    for c in ctx:
-        if c == '-':
-            syms.append('hyph')
-        else:
-            syms.append('"{}".symtab'.format(c))
-    if ctx == '':
-        syms.append('""')
-    return bos + ' '.join(syms) + eos
+    return ' hyph '.join(['"{}"'.format(c) for c in ctx.split('-')])
 
 
 def save_thrax_file(args: argparse.Namespace, hm: HyphenMin, ex: Exceptions, pt: Patterns):
+    # Character conversion tables
+    utf8toascii = {'a': 'a', 'A': 'a', 'b': 'b', 'B': 'b', 'c': 'c', 'C': 'c',
+                   'd': 'd', 'D': 'd', 'e': 'e', 'E': 'e', 'f': 'f', 'F': 'f',
+                   'g': 'g', 'G': 'g', 'h': 'h', 'H': 'h', 'i': 'i', 'I': 'i',
+                   'j': 'j', 'J': 'j', 'k': 'k', 'K': 'k', 'l': 'l', 'L': 'l',
+                   'm': 'm', 'M': 'm', 'n': 'n', 'N': 'n', 'o': 'o', 'O': 'o',
+                   'p': 'p', 'P': 'p', 'q': 'q', 'Q': 'q', 'r': 'r', 'R': 'r',
+                   's': 's', 'S': 's', 't': 't', 'T': 't', 'u': 'u', 'U': 'u',
+                   'v': 'v', 'V': 'v', 'w': 'w', 'W': 'w', 'x': 'x', 'X': 'x',
+                   'y': 'y', 'Y': 'y', 'z': 'z', 'Z': 'z',
+                   'ä': 'A', 'Ä': 'A', 'ö': 'O', 'Ö': 'O', 'ü': 'U', 'Ü': 'U',
+                   'å': 'N', 'Å': 'N', 'ß': 'S', '-': '-'}
+    asciitoutf8 = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f',
+                   'g': 'g', 'h': 'h', 'i': 'i', 'j': 'j', 'k': 'k', 'l': 'l',
+                   'm': 'm', 'n': 'n', 'o': 'o', 'p': 'p', 'q': 'q', 'r': 'r',
+                   's': 's', 't': 't', 'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x',
+                   'y': 'y', 'z': 'z', 'A': 'ä', 'O': 'ö', 'U': 'u', 'N': 'å',
+                   'S': 'ß', '-': '-'}
+    sigmastar = '" | "'.join('abcdefghijklmnopqrstuvwxyzAOUNS-')
     # Split patterns so they contain only one hyphenation point and order them by ascending priority
     rewrites = [[] for i in range(10)]
-    #digits = [0] * 10
+    def convert_pattern_charset(s: str) -> str:
+        l = [c if c.isdigit() or c == '.' else utf8toascii[c] for c in s]
+        return ''.join(l)
     for pattern in pt.patterns:
-        pattern = convert_to_symbols(pattern)
+        pattern = convert_pattern_charset(pattern)
         last_pos = len(pattern)-1
         last_letter_pos = None
         for i in range(last_pos, -1, -1):
@@ -184,28 +188,40 @@ def save_thrax_file(args: argparse.Namespace, hm: HyphenMin, ex: Exceptions, pt:
                     rctx.append(c)
                     if i < last_letter_pos and i < max_hyph_pos:
                         rctx.append('-')
-            #print('{} {} {}'.format(''.join(lctx), pattern[point], ''.join(rctx)))
             rewrites[point_value].append((''.join(lctx), ''.join(rctx)))
-        #digits[sum(c.isdigit() for c in pattern)] += 1
-    #print(digits)
-    #print(rewrites)
-    #for l in rewrites:
-    #    print(len(l))
-    print('symtab = SymbolTable[\'hyph-de.sym\'];', file=args.thraxfile)
-    print('ConvIn = StringFile[\'hyph-de-in.tsv\', \'utf8\', symtab];', file=args.thraxfile)
-    print('ConvOut = StringFile[\'hyph-de-out.tsv\', symtab, \'byte\'];', file=args.thraxfile)
+    # Partition patterns
+    partition_filenames = []
+    partition_rules = []
+    PARTITION_SIZE = 50
+    partition_filename_root, _ = os.path.splitext(args.thraxfile.name)
+    rw = [(priority, lrctxs) for priority, contexts in enumerate(rewrites) for lrctxs in contexts]
+    for partition_index, partition in enumerate([rw[i:i+PARTITION_SIZE] for i in range(0, len(rw), PARTITION_SIZE)], 1):
+        partition_filename = '{}_{}.grm'.format(partition_filename_root, partition_index)
+        partition_filenames.append(partition_filename)
+        partition_rules.append('p{}'.format(partition_index))  # Use it when creating main Thrax file
+        with open(partition_filename, 'w') as partfile:
+            print('hyph = "-" ?;', file=partfile)
+            print('sigmastar = Optimize["{}"]*;'.format(sigmastar), file=partfile)
+            print('export HYPHENATE = Optimize[', file=partfile)
+            cdrewrites = []
+            for priority, (lctx, rctx) in partition:
+                hyph = '"-": ""' if priority % 2 == 0 else '"": "-"'
+                lc = thraxified_context(lctx)
+                rc = thraxified_context(rctx)
+                cdrewrites.append('    CDRewrite[{}, {}, {}, sigmastar]'.format(hyph, lc, rc))
+            print(' @\n'.join(cdrewrites), file=partfile)
+            print('];', file=partfile)
+    for partition_filename, partition_rule in zip(partition_filenames, partition_rules):
+        print('import \'{}\' as {};'.format(partition_filename, partition_rule), file=args.thraxfile)
     print('', file=args.thraxfile)
-    print('hyph = "-".symtab ?;', file=args.thraxfile)
-    print('sigma = StringFile[\'hyph-de-sigma.txt\', symtab];', file=args.thraxfile)
-    print('sigmastar = sigma*;', file=args.thraxfile)
-    print('export HYPHENATE = Optimize[ConvIn @', file=args.thraxfile)
-    for priority, contexts in enumerate(rewrites):
-        for lctx, rctx in contexts:
-            rewrite = '"-": ""' if priority % 2 == 0 else '"": "-"'
-            lcs = thraxified_context(lctx)
-            rcs = thraxified_context(rctx)
-            print('    CDRewrite[{}, {}, {}, sigmastar] @'.format(rewrite, lcs, rcs), file=args.thraxfile)
-    print('    ConvOut];', file=args.thraxfile)
+    utf8toascii_conv = ['("{}": "{}")'.format(k, v) for k, v in utf8toascii.items()]
+    print('input_conv = Optimize[{}];'.format(' | '.join(utf8toascii_conv)), file=args.thraxfile)
+    asciitoutf8_conv = ['("{}": "{}")'.format(k, v) for k, v in asciitoutf8.items()]
+    print('output_conv = Optimize[{}];'.format(' | '.join(asciitoutf8_conv)), file=args.thraxfile)
+    print('export HYPHENATE = input_conv @', file=args.thraxfile)
+    for partition_rule in partition_rules:
+        print('    {}.HYPHENATE @'.format(partition_rule), file=args.thraxfile)
+    print('    output_conv;', file=args.thraxfile)
 
 
 def main():
